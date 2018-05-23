e36299911eb498e9f7ba978d69ddfb033800ed11
carlosamouco <carlosmouco@gmail.com>
2018-05-23 17:54:47 +0100

    MATLAB & Python shared

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 SCRIPT=$(readlink -f "$0")
 PWD=$(dirname "$SCRIPT")
+
 echo "Creating matlab volume..."
 docker volume create --name matlab
+
+echo "Creating ptyhon-site-packages volume..."
+docker volume create --name python-site-packages
+
+echo "Starting containers..."
 docker-compose -f "$PWD"/docker-compose.prod.yml up
