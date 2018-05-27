b9b81d1c8f3057be1a5c4d0db343b9df52bed2c1
unknown <carlosamouco@gmail.com>
2018-05-27 20:57:32 +0100

    power shell script for windows

---
+++ windws.start.ps1
@@ -0,0 +1,14 @@
+$Env:PWD="/$PWD"
+$Env:COMPOSE_CONVERT_WINDOWS_PATHS=1
+
+echo "Creating matlab volume..."
+docker volume create --name matlab
+
+echo "Building docker-sandbox container..."
+docker build -t docker-sandbox ./server/sandbox/
+
+echo "Removing running docker-sandbox containers..."
+docker rm -f $(docker ps -a -q --filter ancestor=docker-sandbox)
+
+echo "Starting containers..."
+docker-compose up
