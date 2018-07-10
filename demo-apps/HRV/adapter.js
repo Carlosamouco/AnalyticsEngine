2d3774dbe4b8b723f6f3074f7672ff0cc2bfa778
unknown <carlosamouco@gmail.com>
2018-07-10 17:41:15 +0100

    final stages

--- demo-apps/HRV/adapter.js
+++ demo-apps/HRV/adapter.js
@@ -2,8 +2,8 @@ const spawn = require("child_process").spawn;
 const fs = require("fs");
 const path = require("path");
 
-if (process.argv.length < 10) {
-    console.log(`usage: ${process.argv[0]} ${process.argv[1]} <HRV App> <outputDir> <HRV File> <Overlap> <Window Time> <Age> <HR Rest> <Time>`);
+if (process.argv.length < 7) {
+    console.log(`usage: ${process.argv[0]} ${process.argv[1]} <HRV App> <outputDir> <HRV File> <Overlap> <Window Time>`);
     process.exit(1);
 }
 
@@ -12,15 +12,26 @@ const outDir = process.argv[3];
 const json = process.argv[4];
 const overlap = process.argv[5];
 const window = process.argv[6];
-const age = process.argv[7];
-const hrRest = process.argv[8];
-const time = process.argv[9];
+let age;
+let hrRest;
+let time;
 
 const file = JSON.parse(fs.readFileSync(json, "utf8"));
 const rr = fs.createWriteStream(path.join(outDir, "intervals.rr"));
 
 for (const row of file.data) {
-    rr.write(row.Value + "\n");
+	if (row.factor_id == "10" && row.literal_id == "32") {
+		rr.write(row.value + "\n");
+		if (!time) {
+			time = row.datetime.split(" ")[1];
+		}
+	}
+	else if (row.factor_id == "3" && row.literal_id == "8") {
+		age = row.value
+	}
+	else if (row.factor_id == "47" && row.literal_id == "13") {
+		hrRest = row.value
+	}
 }
 rr.end();
 
